4b7c5b3bb1ca79e8d1cd70168dac46cdafa3a02a
banma1234 <77619465+banma1234@users.noreply.github.com>
2021-01-25 18:24:28 +0900

    백준 5-7
    평균은 넘겠지

---
+++ 5/5-7(pbs).py
@@ -0,0 +1,10 @@
+case = int(input())
+for i in range(case):
+    arr = list(map(int, input().split()))
+    avg = sum(arr[1:])/arr[0]
+    count = 0
+    for j in arr[1:]:
+        if j > avg:
+            count+=1
+    rate = (count/arr[0]*100)
+    print(f'{rate:.3f}%')
