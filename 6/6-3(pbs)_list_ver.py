923b881a191da0b39c6d693e5b52acf5c36bd3fb
banma1234 <77619465+banma1234@users.noreply.github.com>
2021-01-26 22:52:50 +0900

    백준 6-3(리스트 활용)
    한수

--- 6/6-3(pbs)_list_ver.py
+++ 6/6-3(pbs)_list_ver.py
@@ -4,7 +4,7 @@ def hansu(n):
         return n
     elif 100<=n<=1000:
         for i in range(100, n+1):
-            arr=list(map(int, str(i)))
+            arr=list(map(int, str(i)))  # map 함수의 활용 : 문자인 i를 int로 치환, arr 리스트에 쪼개서 담았다.
             if arr[0] - arr[1] == arr[1] - arr[2]:
                 count+=1
         return 99+count
