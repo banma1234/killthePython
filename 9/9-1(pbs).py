a41e698624ceff0229d9c1feadf2d5625fa4f486
banma1234 <77619465+banma1234@users.noreply.github.com>
2021-02-01 19:10:38 +0900

    백준 9-1
    소수 찾기

---
+++ 9/9-1(pbs).py
@@ -0,0 +1,13 @@
+case = int(input())
+num = list(map(int, input().split()))
+res = 0
+for i in num:
+    count = 0
+    if i == 1:
+        continue
+    for j in range(2, i+1):
+        if i % j == 0:
+            count += 1
+    if count == 1:
+        res+=1
+print(res)
